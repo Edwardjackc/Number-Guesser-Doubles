286f01ec0075e63d8dae080c3f23bb0d066e06b2
Patrick Goulding <patrickgoulding@gmail.com>
2019-03-20 18:00:48 -0600

    added function to check for winner
    Co-Authored-by: Ed Cheatham<edwardjackc@gmail.com>

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ var outputNameCh1 = document.querySelector('#name-output-challenger1');
 var outputNameCh2 = document.querySelector('#name-output-challenger2');
 var outputGuessCh1 = document.querySelector('#guess-output-challenger1');
 var outputGuessCh2 = document.querySelector('#guess-output-challenger2');
+var outputHighLow1 = document.querySelector('#guess-output-high-low1');
+var outputHighLow2 = document.querySelector('#guess-output-high-low2');
 /*--------- Buttons --------------------*/
 var btnUpdateRange = document.querySelector('#btn-update');
 var btnSubmit = document.querySelector('#btn-submit');
@@ -21,6 +23,11 @@ var btnReset = document.querySelector('#btn-reset');
 var btnClear = document.querySelector('#btn-clear');
 
 /*---------- Global Variables ----------*/
+var min = inputRangeMin.value || 1;
+var max = inputRangeMax.value || 100;
+var outputWinner;
+var randomNum = Math.floor(Math.random()*(+max - +min)) + +min;
+console.log(randomNum);
 
 
 /*---------- Event Listeners -----------*/
@@ -31,13 +38,42 @@ btnSubmit.addEventListener('click', playGame);
 
 
 /*---------- Functions -----------------*/
-function getRandom(min, max){
-  max = inputRangeMin.value;
-  min = inputRangeMax.value;
-  return Math.floor(Math.random() * (max-min+1) ) + min;
+
+function checkForCh1() {
+  if(inputGuessCh1.value == randomNum) { 
+     outputWinner = inputNameCh1.value;
+     outputHighLow1.innerText = 'BOOM';
+     appendCard();
+  } else if (inputGuessCh1.value < randomNum) {
+    outputHighLow1.innerText = 'TO LOW';
+  } else { 
+    outputHighLow1.innerText = 'TO HIGH';
+    }
+}
+ 
+ function checkForCh2() {
+  if(inputGuessCh2.value == randomNum) { 
+     outputWinner = inputNameCh2.value;
+     outputHighLow2.innerText = 'BOOM';
+     appendCard();
+  } else if (inputGuessCh2.value < randomNum) {
+    outputHighLow2.innerText = 'TO LOW';
+  } else { 
+    outputHighLow2.innerText = 'TO HIGH';
+    }
 }
 
-console.log()
+  function checkWinner() {
+    checkForCh1();
+    checkForCh2();
+  }
+
+// function getRandom(min, max){
+//   max = inputRangeMin.value;
+//   min = inputRangeMax.value;
+//   return Math.floor(Math.random() * (max-min+1) ) + min;
+// }
+
 function updateRange(e) {
   e.preventDefault();
   outputRangeMin.innerText = inputRangeMin.value;
@@ -48,7 +84,8 @@ function playGame(e) {
   e.preventDefault();
   checkAllInputs();
   displayNames();
-  appendCard();
+  checkWinner();
+  randomNum;
 }; 
 
 function displayNames(){
@@ -74,7 +111,7 @@ function appendCard(){
       </div>
       <hr>
       <div class="card-output-results">
-        <h2 class="winner">${inputNameCh1.value}</h2>
+        <h2 class="winner">${outputWinner}</h2>
         <p class="card-style-winner">Winner</p>
       </div>
       <hr>
@@ -129,4 +166,4 @@ function checkValidInputGuess2() {
 
 
 
-window.load(getRandom())
+// window.load(getRandom())
